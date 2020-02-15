e1879e007b80463d0b81485cde9563f7cb9c51d2
Toby Jones <25872549+monotron@users.noreply.github.com>
2020-02-15 13:10:01 +0000

    initial commit

---
+++ src/main/java/me/monotron/screwthisgame/backend/ScrewThisGameBackendApplication.java
@@ -0,0 +1,12 @@
+package me.monotron.screwthisgame.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewThisGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewThisGameBackendApplication.class, args);
+    }
+}
