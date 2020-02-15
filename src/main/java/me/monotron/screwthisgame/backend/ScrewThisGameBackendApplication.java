7c53a10395f0374248e9582b741f3341e09e4caf
Toby Jones <25872549+monotron@users.noreply.github.com>
2020-02-15 17:16:57 +0000

    enable mongo repositories

--- src/main/java/me/monotron/screwthisgame/backend/ScrewThisGameBackendApplication.java
+++ src/main/java/me/monotron/screwthisgame/backend/ScrewThisGameBackendApplication.java
@@ -2,8 +2,9 @@ package me.monotron.screwthisgame.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "me.monotron.screwthisgame.backend")
 @SpringBootApplication(scanBasePackages = "me.monotron.screwthisgame.backend")
 public class ScrewThisGameBackendApplication {
 
