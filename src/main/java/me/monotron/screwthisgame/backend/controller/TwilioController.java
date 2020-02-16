7a59549cf5b4fa2703437cc61f908ee088ded0c9
Toby Jones <25872549+monotron@users.noreply.github.com>
2020-02-16 10:37:48 +0000

    add stub twilio controller

--- src/main/java/me/monotron/screwthisgame/backend/controller/TwilioController.java
+++ src/main/java/me/monotron/screwthisgame/backend/controller/TwilioController.java
@@ -1,4 +1,20 @@
 package me.monotron.screwthisgame.backend.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@Slf4j
+@AllArgsConstructor
 public class TwilioController {
+
+    @PostMapping(value = "/twilio/receive")
+    public ResponseEntity<String> receiveMessage(@RequestBody String body) {
+        log.info(body);
+        return ResponseEntity.ok("");
+    }
 }
