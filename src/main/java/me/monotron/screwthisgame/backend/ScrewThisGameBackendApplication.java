0d80f16813dbf99fa1a93486e6324e0e4a549de1
Toby Jones <25872549+monotron@users.noreply.github.com>
2020-02-16 10:48:30 +0000

    add twilio init

--- src/main/java/me/monotron/screwthisgame/backend/ScrewThisGameBackendApplication.java
+++ src/main/java/me/monotron/screwthisgame/backend/ScrewThisGameBackendApplication.java
@@ -1,17 +1,32 @@
 package me.monotron.screwthisgame.backend;
 
+import com.twilio.Twilio;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(scanBasePackages = "me.monotron.screwthisgame.backend")
 @EnableMongoRepositories(basePackages = "me.monotron.screwthisgame.backend")
 @EnableScheduling
 public class ScrewThisGameBackendApplication {
 
+    @Value("${twilio.account-sid}")
+    String twilioSid;
+
+    @Value("${twilio.auth-token}")
+    String authToken;
+
     public static void main(String[] args) {
         SpringApplication.run(ScrewThisGameBackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void initTwilio() {
+        Twilio.init(twilioSid, authToken);
     }
 }
--- src/main/java/me/monotron/screwthisgame/backend/controller/TwilioController.java
+++ src/main/java/me/monotron/screwthisgame/backend/controller/TwilioController.java
@@ -1,5 +1,6 @@
 package me.monotron.screwthisgame.backend.controller;
 
+import com.twilio.Twilio;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,6 @@ public class TwilioController {
 
     @PostMapping(value = "/twilio/receive")
     public ResponseEntity<String> receiveMessage(@RequestBody String body) {
-        log.info(body);
-        return ResponseEntity.ok("");
+
     }
 }
